78a2cb289be50647af8490043dd5217ed8d88bfe
Alberto Donato <alberto.donato@gmail.com>
2015-03-23 15:26:46 +0200

    Use config file.

--- setup.py
+++ setup.py
@@ -1,39 +1,39 @@
 #
-# This file is part of ${title}.
+# This file is part of ${project.title}.
 
-# ${title} is free software: you can redistribute it and/or modify it under
+# ${project.title} is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 
-# ${title} is distributed in the hope that it will be useful, but WITHOUT ANY
+# ${project.title} is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 # A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with ${title}.  If not, see <http://www.gnu.org/licenses/>.
+# along with ${project.title}.  If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup, find_packages
 
-from ${name} import __version__, __doc__ as description
+from ${project.name} import __version__, __doc__ as description
 
 
 config = {
-    'name': '${name}',
+    'name': '${project.name}',
     'version': __version__,
     'license': 'GPLv3+',
     'description': description,
     'long_description': open('README.rst').read(),
-    'author': 'Alberto Donato',
-    'author_email': 'alberto.donato@gmail.com',
-    'maintainer': 'Alberto Donato',
-    'maintainer_email': 'alberto.donato@gmail.com',
-    'url': 'https://bitbucket.org/ack/${name}',
-    'download_url': 'https://bitbucket.org/ack/${name}/downloads',
-    'packages': find_packages(exclude=['*.test.*', '*.test', 'test.*']),
+    'author': '${author.name}',
+    'author_email': '${author.email}',
+    'maintainer': '${author.name}',
+    'maintainer_email': '${author.email}',
+    'url': '${project.url}',
+    'download_url': '${project.download-url}',
+    'packages': find_packages(exclude=['*.tests.*', '*.tests', 'tests.*']),
     'include_package_data': True,
     'entry_points': {'console_scripts': []},
-    'test_suite': '${name}',
+    'test_suite': '${project.name}',
     'install_requires': [],
     'tests_require': [],
     'keywords': '',

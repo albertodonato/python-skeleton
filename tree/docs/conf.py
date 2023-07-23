ba56dfa941218e2192e99a931fb37a3b72ce8130
Alberto Donato <alberto.donato@gmail.com>
2023-07-23 22:27:52 +0200

    update docs config

--- tree/docs/conf.py
+++ tree/docs/conf.py
@@ -1,26 +1,23 @@
-# -*- coding: utf-8 -*-
-#
-# ${project.name} documentation build configuration file.
-#
-# This file is execfile()d with the current directory set to its
-# containing dir.
-#
-
 from datetime import datetime
-import os
 import sys
-
+import os
 # Add the directory containing the project tree so that the autodocs extension
 # can find the code.
 sys.path.insert(0, os.path.abspath(".."))
 # Import the base module.
 import ${project.package}
 
-# -- General configuration ------------------------------------------------
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+project = "${project.name}"
+copyright = "{}, ${author.name}".format(datetime.today().year)
+author = "${author.name}"
+release = str(snaphelpers.__version__)
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.todo",
@@ -29,70 +26,11 @@ extensions = [
     "sphinx_autodoc_typehints",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
+exclude_patterns = []
 
-# The suffix of source filenames.
-source_suffix = ".rst"
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The master toctree document.
-master_doc = "index"
-
-# General information about the project.
-project = "${project.name}"
-copyright = "{}, ${author.name}".format(datetime.today().year)
-
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-release = str(${project.package}.__version__)
-# The full version, including alpha/beta/rc tags.
-version = release
-
-# There are two options for replacing |today|: either, you set today to some
-# non-false value, then it is used:
-#  today = ''
-# Else, today_fmt is used as the format for a strftime call.
-#  today_fmt = '%B %d, %Y'
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
-
-
-# -- Options for HTML output ----------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.
 html_theme = "default"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-# html_use_opensearch = ''
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = "${project.package}doc"
-
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (
-        "index",
-        "${project.package}",
-        "${project.name} Documentation",
-        ["${author.name}"],
-        1,
-    )
-]
-
-# If true, show URL addresses after external links.
-#  man_show_urls = False
